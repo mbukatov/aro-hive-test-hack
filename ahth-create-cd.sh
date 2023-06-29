cd748d51ddbdaa90e2a0f591df4785da354d892e
Martin Bukatovič <email = mbukatov@redhat.com>
2023-06-29 17:40:44 +0200

    ahth-create-cd: create multiple clusters

--- ahth-create-cd.sh
+++ ahth-create-cd.sh
@@ -3,13 +3,13 @@
 show_help()
 {
   echo "Create ClusterDeployment CR based on given json template"
-  echo "Usage: $(basename "$0") [-d] -t TEMPLATE -n ARO-NAMESPACE"
+  echo "Usage: $(basename "$0") [-d] -t TEMPLATE -n ARO-NAMESPACE [-s START] [-e END]"
 }
 
 create_cd()
 {
-  TEMPLATE=$1
-  NS=$2
+  local TEMPLATE=$1
+  local NS=$2
   if ! oc get ns/"${NS}" > /dev/null; then
     $DEBUG oc create namespace $NS
   fi
@@ -27,19 +27,33 @@ fi
 
 unset DEBUG
 
-while getopts "dn:t:h" OPT; do
+while getopts "dn:t:s:e:h" OPT; do
   case $OPT in
   d) DEBUG=echo;;
   n) NS=$OPTARG;;
   t) TEMPLATE=$OPTARG;;
+  s) N_START=$OPTARG;;
+  e) N_END=$OPTARG;;
   h) show_help; exit;;
   esac
 done
 
 shift $(($OPTIND-1))
 
+if [[ ! $TEMPLATE ]]; then
+  echo "error: template not specified, use -t option" >&2
+  exit 1
+fi
+
 if [[ ! $DEBUG ]]; then
   set -ex
 fi
 
-create_cd ${TEMPLATE} ${NS}
+if [[ ${N_START} -gt 0 && ${N_END} -gt ${N_START} ]]; then
+  for i in $(seq -f "%05g" $N_START $N_END); do
+    NS_I=${NS}-${i}
+    create_cd ${TEMPLATE} ${NS_I}
+  done
+else
+  create_cd ${TEMPLATE} ${NS}
+fi
